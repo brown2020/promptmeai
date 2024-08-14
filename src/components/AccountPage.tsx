046cc110fcc707124a8c3967947470c11755aa08
Steve Brown <brown2020@gmail.com>
2024-08-14 11:38:06 -0700

    formatting

--- src/components/AccountPage.tsx
+++ src/components/AccountPage.tsx
@@ -7,8 +7,6 @@ import PaymentsPage from "./PaymentsPage";
 import AuthDataDisplay from "./AuthDataDisplay";
 
 export default function AccountPage() {
-  const uid = useAuthStore((state) => state.uid);
-
   return (
     <div className="flex flex-col h-full w-full max-w-4xl mx-auto gap-4">
       <div className="text-3xl font-bold">User Profile</div>
