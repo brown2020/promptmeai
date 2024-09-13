50f8c602598f74540565249e14cf860eaab31888
Muhamad Risman <real.risman@gmail.com>
2024-09-13 14:58:33 +0700

    feat: add initial v2 chat app

---
+++ src/app/v2/chat/page.tsx
@@ -0,0 +1,5 @@
+const ChatPage = () => {
+  return <div className="flex h-screen w-screen">Initial v2 chat page</div>;
+};
+
+export default ChatPage;
---
+++ src/app/v2/layout.tsx
@@ -0,0 +1,17 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
+import "../globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
