e27af4a0c5d689ce31829adc47ddf62b50b53cbf
Muhamad Risman <real.risman@gmail.com>
2024-09-14 23:17:40 +0700

    feat: add ButtonIcon component

---
+++ src/components/v2/ButtonIcon.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/utils/tailwind";
+import { PropsWithChildren } from "react";
+import { IconType } from "react-icons/lib";
+
+type ButtonIconProps = {
+  icon: IconType;
+  type?: "default" | "primary" | "secondary";
+  isActive?: boolean;
+  iconSize?: number;
+} & PropsWithChildren;
+
+const ButtonIcon = ({
+  icon: Icon,
+  type = "default",
+  isActive = false,
+  iconSize = 18,
+}: ButtonIconProps) => {
+  return (
+    <div
+      className={cn(
+        "bg-[#F9F9F9] rounded-lg w-[32px] h-[32px] flex justify-center items-center shadow-xl cursor-pointer",
+        {
+          "bg-[#10A37F]": isActive,
+          "bg-[#23C69E]": type === "primary",
+          "bg-[#EFEFEF]": type === "secondary",
+        }
+      )}
+    >
+      <Icon
+        className={cn("text-[#255148]", {
+          "text-white": isActive || type === "primary",
+          "text-[#3B3B3B]": type === "secondary",
+        })}
+        size={iconSize}
+      />
+    </div>
+  );
+};
+
+export default ButtonIcon;
