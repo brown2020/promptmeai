c3bc6f024e7b023a1002f9538c1bdaeceadb92e0
Muhamad Risman <real.risman@gmail.com>
2024-09-16 21:58:58 +0700

    feat: integrate ChatInput with the services

--- src/screens/chat/components/ChatInput.tsx
+++ src/screens/chat/components/ChatInput.tsx
@@ -1,8 +1,104 @@
+"use client";
+
+import { continueConversation } from "@/actions/generateActions";
+import { MODEL_NAMES } from "@/constants/modelNames";
+import { Message, saveChat, updateChat } from "@/services/chatService";
+import { useChatSideBarStore } from "@/zustand/useChatSideBarStore";
+import { useChatStore } from "@/zustand/useChatStore";
+import { useUser } from "@clerk/nextjs";
+import { CoreMessage } from "ai";
+import { readStreamableValue } from "ai/rsc";
+import { useCallback, useState } from "react";
 import { ImMagicWand } from "react-icons/im";
 import { IoMicOutline } from "react-icons/io5";
 import { PiPaperPlaneTilt } from "react-icons/pi";
 
 const ChatInput = () => {
+  const { user } = useUser();
+  const { messages, addMessage } = useChatStore((state) => state);
+  const { activeChatId, setActiveChatId } = useChatSideBarStore(
+    (state) => state
+  );
+
+  const [input, setInput] = useState<string>("");
+
+  const getAssistantResponse = useCallback(
+    async (model: string, userMessage: CoreMessage) => {
+      const currentMessages: CoreMessage[] = messages.flatMap((message) => [
+        message.userMessage,
+        ...Object.values(message.responses),
+      ]);
+
+      const result = await continueConversation(
+        [...currentMessages, userMessage],
+        model
+      );
+
+      for await (const content of readStreamableValue(result)) {
+        useChatStore.getState().addResponse(model, {
+          role: "assistant",
+          content: content as string,
+        });
+      }
+    },
+    [messages]
+  );
+
+  const saveChatFunction = useCallback(
+    async (coreMessages: CoreMessage[]) => {
+      if (user?.id) {
+        const formattedMessages: Message[] = coreMessages.map(
+          (coreMessage) => ({
+            userMessage: coreMessage,
+            responses: {},
+          })
+        );
+
+        try {
+          if (activeChatId) {
+            await updateChat(user.id, activeChatId, formattedMessages);
+          } else {
+            const chatId = await saveChat(
+              user.id,
+              user.fullName || "",
+              formattedMessages
+            );
+            if (chatId) {
+              setActiveChatId(chatId);
+            }
+          }
+        } catch (error) {
+          console.error("Error saving or updating chat: ", error);
+        }
+      }
+    },
+    [activeChatId, user?.id, user?.fullName, setActiveChatId]
+  );
+
+  const submitHandler = async () => {
+    if (!input) return;
+
+    const newUserMessage: CoreMessage = { content: input, role: "user" };
+    addMessage(newUserMessage);
+    setInput("");
+
+    try {
+      await Promise.all(
+        MODEL_NAMES.map((model) =>
+          getAssistantResponse(model.value, newUserMessage)
+        )
+      );
+
+      const updatedMessages: CoreMessage[] = useChatStore
+        .getState()
+        .messages.map((msg) => msg.userMessage);
+
+      await saveChatFunction(updatedMessages);
+    } catch (error) {
+      console.error("Error handling submission: ", error);
+    }
+  };
+
   return (
     <div className="self-end w-full max-w-[720px] h-[56px] flex-shrink-0 flex gap-[16px] justify-center items-center">
       <div className="w-full bg-white rounded-xl shadow p-[12px] flex gap-[12px] items-center">
@@ -12,6 +108,9 @@ const ChatInput = () => {
         <input
           className="w-full text-[14px] text-[#A0A7BB] outline-none"
           placeholder="Ask questions, or type '/' for commands"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && submitHandler()}
         />
         <div className="flex items-center justify-center h-[32px] w-[32px] rounded-lg cursor-pointer flex-shrink-0">
           <PiPaperPlaneTilt size={18} color="#ABABAB" />
