22b1ba8b8ebd1f49b037665b4dd5ec4822528e63
Muhamad Risman <real.risman@gmail.com>
2024-09-16 22:31:49 +0700

    feat: add chat response empty state

---
+++ src/screens/chat/components/ChatResponseEmptyState.tsx
@@ -0,0 +1,47 @@
+import { useUser } from "@clerk/nextjs";
+import { MdWavingHand } from "react-icons/md";
+import { motion } from "framer-motion";
+
+const ChatResponseEmptyState = () => {
+  const { user } = useUser();
+
+  return (
+    <div className="h-full flex flex-col justify-center items-center gap-1">
+      <motion.div
+        className="bg-[#464646] p-3 rounded-2xl mb-4"
+        initial={{ scale: 0 }}
+        animate={{
+          scale: [1, 1.2, 1],
+          transition: { duration: 0.6 },
+        }}
+      >
+        <MdWavingHand color="#E8E8E8" size={32} />
+      </motion.div>
+
+      <motion.span
+        className="text-xl text-[#9F969C]"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{
+          opacity: 1,
+          y: 0,
+          transition: { duration: 0.5, delay: 0.2 },
+        }}
+      >
+        Hi, {user && user.fullName}
+      </motion.span>
+      <motion.span
+        className="text-xl text-[#53494D]"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{
+          opacity: 1,
+          y: 0,
+          transition: { duration: 0.5, delay: 0.4 },
+        }}
+      >
+        Can I help you with anything?
+      </motion.span>
+    </div>
+  );
+};
+
+export default ChatResponseEmptyState;
--- src/screens/chat/components/ChatResponseList.tsx
+++ src/screens/chat/components/ChatResponseList.tsx
@@ -1,108 +1,16 @@
-import ChatResponseCard from "./ChatResponseCard";
+"use client";
+
+import { useChatStore } from "@/zustand/useChatStore";
+import ChatResponseEmptyState from "./ChatResponseEmptyState";
 
 const ChatResponseList = () => {
+  const messages = useChatStore((state) => state.messages);
+
+  console.log("messages", messages);
+
   return (
     <div className="h-full w-full overflow-y-auto flex flex-col gap-[24px] pr-[8px] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
-      <ChatResponseCard
-        type="self"
-        date="24 Sep - 11:30 PM"
-        response="How do you define usability testing in UX design?"
-      />
-
-      {/* Open AI response */}
-      <ChatResponseCard
-        type="ai"
-        aiModel="gpt"
-        date="24 Sep - 11:30 PM"
-        response="Sure! Here are three different versions of 404 error messages
-        for an ecommerce clothing website: Uh-oh! It looks like the
-        page you're looking for isn't here. Please check the URL and
-        try again or return to the homepage to continue shopping. 2.
-        Whoops! We can't seem to find the page you're looking for.
-        Please double-check the URL or use our search bar to find what
-        you need. You can also browse our collection of stylish
-        clothes and accessories. 3. Sorry, the page you're trying to
-        access isn't available. It's possible that the item has sold
-        out or the page has been removed. Please click back to return
-        to the previous page or head over to our homepage to explore
-        more."
-      />
-
-      {/* Gemini AI response */}
-      <ChatResponseCard
-        type="ai"
-        aiModel="gemini"
-        date="24 Sep - 11:30 PM"
-        response="Sure! Here are three different versions of 404 error messages
-        for an ecommerce clothing website: Uh-oh! It looks like the
-        page you're looking for isn't here. Please check the URL and
-        try again or return to the homepage to continue shopping. 2.
-        Whoops! We can't seem to find the page you're looking for.
-        Please double-check the URL or use our search bar to find what
-        you need. You can also browse our collection of stylish
-        clothes and accessories. 3. Sorry, the page you're trying to
-        access isn't available. It's possible that the item has sold
-        out or the page has been removed. Please click back to return
-        to the previous page or head over to our homepage to explore
-        more."
-      />
-
-      {/* Mistral AI response */}
-      <ChatResponseCard
-        type="ai"
-        aiModel="mistral"
-        date="24 Sep - 11:30 PM"
-        response="Sure! Here are three different versions of 404 error messages
-        for an ecommerce clothing website: Uh-oh! It looks like the
-        page you're looking for isn't here. Please check the URL and
-        try again or return to the homepage to continue shopping. 2.
-        Whoops! We can't seem to find the page you're looking for.
-        Please double-check the URL or use our search bar to find what
-        you need. You can also browse our collection of stylish
-        clothes and accessories. 3. Sorry, the page you're trying to
-        access isn't available. It's possible that the item has sold
-        out or the page has been removed. Please click back to return
-        to the previous page or head over to our homepage to explore
-        more."
-      />
-
-      {/* Claude AI response */}
-      <ChatResponseCard
-        type="ai"
-        aiModel="claude"
-        date="24 Sep - 11:30 PM"
-        response="Sure! Here are three different versions of 404 error messages
-        for an ecommerce clothing website: Uh-oh! It looks like the
-        page you're looking for isn't here. Please check the URL and
-        try again or return to the homepage to continue shopping. 2.
-        Whoops! We can't seem to find the page you're looking for.
-        Please double-check the URL or use our search bar to find what
-        you need. You can also browse our collection of stylish
-        clothes and accessories. 3. Sorry, the page you're trying to
-        access isn't available. It's possible that the item has sold
-        out or the page has been removed. Please click back to return
-        to the previous page or head over to our homepage to explore
-        more."
-      />
-
-      {/* LLaMA 3.1 405B AI response */}
-      <ChatResponseCard
-        type="ai"
-        aiModel="llama"
-        date="24 Sep - 11:30 PM"
-        response="Sure! Here are three different versions of 404 error messages
-        for an ecommerce clothing website: Uh-oh! It looks like the
-        page you're looking for isn't here. Please check the URL and
-        try again or return to the homepage to continue shopping. 2.
-        Whoops! We can't seem to find the page you're looking for.
-        Please double-check the URL or use our search bar to find what
-        you need. You can also browse our collection of stylish
-        clothes and accessories. 3. Sorry, the page you're trying to
-        access isn't available. It's possible that the item has sold
-        out or the page has been removed. Please click back to return
-        to the previous page or head over to our homepage to explore
-        more."
-      />
+      {messages.length === 0 && <ChatResponseEmptyState />}
     </div>
   );
 };
