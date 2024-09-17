23ae9d9487eea8e005dac56e17515da179e9875c
Muhamad Risman <real.risman@gmail.com>
2024-09-17 08:21:37 +0700

    fix: remove console log on response list

--- src/screens/chat/components/ChatResponseList.tsx
+++ src/screens/chat/components/ChatResponseList.tsx
@@ -9,8 +9,6 @@ import { MODEL_NAMES } from "@/constants/modelNames";
 const ChatResponseList = () => {
   const messages = useChatStore((state) => state.messages);
 
-  console.log("messages", messages);
-
   return (
     <div className="h-full w-full overflow-y-auto flex flex-col gap-[24px] pr-[8px] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
       {messages.length === 0 && <ChatResponseEmptyState />}
