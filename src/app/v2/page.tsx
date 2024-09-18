044f95372c7045ed3a87712ca50e2209b7613e67
Muhamad Risman <real.risman@gmail.com>
2024-09-19 04:44:19 +0700

    feat: add auth checker on home screen v2

--- src/app/v2/page.tsx
+++ src/app/v2/page.tsx
@@ -1,6 +1,14 @@
 import HomeScreen from "@/screens/home";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const HomePage = () => {
+  const { userId } = auth();
+
+  if (userId) {
+    return redirect("/v2/chat");
+  }
+
   return <HomeScreen />;
 };
 
