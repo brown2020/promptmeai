6d75f74bd6fe610077390727533b3473d52b922f
Muhamad Risman <real.risman@gmail.com>
2024-09-19 04:34:53 +0700

    feat: connect signin button with clerk

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,9 +1,22 @@
 "use client";
 
+import { SignedOut } from "@clerk/clerk-react";
+import { SignInButton } from "@clerk/nextjs";
 import { motion } from "framer-motion";
 import { GiBrainTentacle } from "react-icons/gi";
+import { useEffect, useState } from "react";
 
 const HomeScreen = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <div className="h-screen w-screen flex justify-center items-center">
       <div className="flex flex-col gap-4 max-w-[450px] prose justify-center items-center text-center">
@@ -41,19 +54,23 @@ const HomeScreen = () => {
           each model, or learn more about how Prompt.me can help you make
           informed decisions in your AI projects.
         </motion.p>
-        <motion.button
-          initial={{ opacity: 0 }}
-          animate={{
-            opacity: 1,
-            y: 0,
-            transition: { duration: 0.5, delay: 0.6 },
-          }}
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95 }}
-          className="w-full h-[50px] bg-[#1A8F70] text-white rounded-lg text-[18px] font-semibold shadow-md hover:bg-[#166854] focus:outline-none focus:ring-2 focus:ring-[#1A8F70] focus:ring-opacity-50 transition-all duration-300 ease-in-out"
-        >
-          Sign in
-        </motion.button>
+        <SignedOut>
+          <SignInButton forceRedirectUrl="/v2/chat" mode="modal">
+            <motion.button
+              initial={{ opacity: 0 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: { duration: 0.5, delay: 0.6 },
+              }}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              className="w-full h-[50px] bg-[#1A8F70] text-white rounded-lg text-[18px] font-semibold shadow-md hover:bg-[#166854] focus:outline-none focus:ring-2 focus:ring-[#1A8F70] focus:ring-opacity-50 transition-all duration-300 ease-in-out"
+            >
+              Sign in
+            </motion.button>
+          </SignInButton>
+        </SignedOut>
       </div>
     </div>
   );
