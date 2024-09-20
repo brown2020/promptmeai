090fb401c4f7e8435487102fcbbb04e777dced06
Muhamad Risman <real.risman@gmail.com>
2024-09-20 22:21:03 +0700

    feat: add navigation logic on LeftPanel component

--- src/layouts/LeftPanel.tsx
+++ src/layouts/LeftPanel.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { PiChatsCircle } from "react-icons/pi";
 import { RiListSettingsFill } from "react-icons/ri";
 import Logo from "@/components/v2/Logo";
@@ -5,16 +7,26 @@ import ButtonIcon from "@/components/v2/ButtonIcon";
 import Divider from "@/components/v2/Divider";
 import ThemeSwitcher from "@/components/v2/ThemeSwitcher";
 import UserProfileButton from "@/components/v2/UserProfileButton";
+import { useRouter } from "next/navigation";
 
 const LeftPanel = () => {
+  const router = useRouter();
+
   return (
     <div className="flex flex-col justify-between items-center w-[64px] flex-shrink-0 bg-white border-r border-[#EAEAEA] p-[16px]">
       {/* Top section */}
       <div className="flex flex-col gap-[64px] justify-center items-center">
         <Logo />
         <div className="flex flex-col gap-[24px] justify-center items-center">
-          <ButtonIcon icon={PiChatsCircle} isActive />
-          <ButtonIcon icon={RiListSettingsFill} />
+          <ButtonIcon
+            icon={PiChatsCircle}
+            isActive
+            onClick={() => router.push("/v2/chat")}
+          />
+          <ButtonIcon
+            icon={RiListSettingsFill}
+            onClick={() => router.push("/v2/settings")}
+          />
         </div>
       </div>
 
