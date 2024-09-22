9779709bf21308e1f06f23fd239b2b609edea605
Muhamad Risman <real.risman@gmail.com>
2024-09-22 22:14:06 +0700

    feat: add initial settings screen

--- src/screens/settings/SettingsScreen.tsx
+++ src/screens/settings/SettingsScreen.tsx
@@ -1,5 +1,24 @@
 const SettingsScreen = () => {
-  return <h1>Settings Screen</h1>;
+  return (
+    <div className="w-screen h-screen bg-white flex justify-center">
+      <div className="bg-[#F5F5F5] w-full rounded-lg flex justify-center m-6 p-8">
+        <div className="max-w-[896px] w-full bg-white rounded-xl shadow px-[16px] py-[12px] flex flex-col gap-[12px]">
+          <h2 className="font-medium text-[20px]">Account Settings</h2>
+          <div className="border rounded-md p-4">
+            <div>
+              <span>Usage Credits: 1000</span>
+              <div>
+                <button>Buy 10,000 Credits</button>
+                <p>
+                  You can either buy credits or add your own API keys bellow.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SettingsScreen;
