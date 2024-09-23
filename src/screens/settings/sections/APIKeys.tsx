7e1caf51f04d0c91dc2da78f366e0d3b4adf7f9d
Muhamad Risman <real.risman@gmail.com>
2024-09-23 14:30:24 +0700

    feat: update style APIKeys section to match with the credit usage

--- src/screens/settings/sections/APIKeys.tsx
+++ src/screens/settings/sections/APIKeys.tsx
@@ -1,15 +1,19 @@
+import Button from "@/components/v2/Button";
 import CardContent from "@/components/v2/CardContent";
 import Input from "@/components/v2/Input";
 
 const APIKeys = () => {
   return (
     <CardContent title="API Keys" overrideStyles="xl:col-span-2">
-      <Input title="Open AI" />
-      <Input title="Anthropic" />
-      <Input title="Google Generative AI" />
-      <Input title="Mistral" />
-      <Input title="Fireworks" />
-      <Input title="Grooq" />
+      <div className="grid xl:grid-rows-3 xl:grid-flow-col gap-4">
+        <Input title="Open AI" />
+        <Input title="Anthropic" />
+        <Input title="Google Generative AI" />
+        <Input title="Mistral" />
+        <Input title="Fireworks" />
+        <Input title="Grooq" />
+      </div>
+      <Button disabled>Update API Keys</Button>
     </CardContent>
   );
 };
