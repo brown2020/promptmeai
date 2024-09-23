c67fa20c3331a8dba547f78dca8dde115d1186ed
Muhamad Risman <real.risman@gmail.com>
2024-09-23 11:07:24 +0700

    feat: add CardContent component

---
+++ src/components/v2/CardContent.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/utils/tailwind";
+import { PropsWithChildren } from "react";
+
+type CardContentProps = {
+  title: string;
+  overrideStyles?: string;
+} & PropsWithChildren;
+
+const CardContent = ({ title, overrideStyles, children }: CardContentProps) => {
+  return (
+    <div
+      className={cn(
+        "border border-[#E5EAEE] p-4 rounded-lg flex flex-col gap-4",
+        overrideStyles
+      )}
+    >
+      <h3 className="text-[20px]">{title}</h3>
+      {children}
+    </div>
+  );
+};
+
+export default CardContent;
