b41cacf9e560d5e8ec4af8aa31df442f7f3f33c6
Muhamad Risman <real.risman@gmail.com>
2024-09-23 14:36:04 +0700

    feat: add max width for larger screen

--- src/screens/settings/SettingsScreen.tsx
+++ src/screens/settings/SettingsScreen.tsx
@@ -7,12 +7,14 @@ const SettingsScreen = () => {
     <div className="w-screen h-screen bg-white relative">
       {/* Green layer */}
       <div className="w-full bg-[#24C69E] h-[235px] rounded-b-2xl relative" />
-      <div className="absolute top-5 left-[10px] bg-[white] shadow w-[calc(100%-64px)] h-[calc(100%-64px)] rounded-2xl flex flex-col gap-5 m-6 p-8">
-        <h2 className="font-medium text-[24px]">Account Settings</h2>
-        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 overflow-auto scrollable-container">
-          <CreditInformation />
-          <APIKeys />
-          <PaymentHistories />
+      <div className="absolute top-5 left-[10px] bg-[white] shadow w-[calc(100%-64px)] h-[calc(100%-64px)] rounded-2xl m-6 flex justify-center">
+        <div className="flex flex-col gap-5 p-8 max-w-[1320px] w-full">
+          <h2 className="font-medium text-[24px]">Account Settings</h2>
+          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 overflow-auto scrollable-container">
+            <CreditInformation />
+            <APIKeys />
+            <PaymentHistories />
+          </div>
         </div>
       </div>
     </div>
