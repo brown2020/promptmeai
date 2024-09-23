84827d62518c53804684a7e016c0b0fc62605205
Muhamad Risman <real.risman@gmail.com>
2024-09-23 10:54:26 +0700

    fix: responsiveness of settings screen

--- src/screens/settings/SettingsScreen.tsx
+++ src/screens/settings/SettingsScreen.tsx
@@ -8,9 +8,9 @@ const SettingsScreen = () => {
     <div className="w-screen h-screen bg-white relative">
       {/* Green layer */}
       <div className="w-full bg-[#24C69E] h-[235px] rounded-b-2xl relative" />
-      <div className="absolute top-5 left-[10px] bg-[white] shadow w-[calc(100%-64px)] h-[calc(100%-64px)] rounded-2xl flex flex-col gap-5 m-6 p-8 ">
+      <div className="absolute top-5 left-[10px] bg-[white] shadow w-[calc(100%-64px)] h-[calc(100%-64px)] rounded-2xl flex flex-col gap-5 m-6 p-8">
         <h2 className="font-medium text-[24px]">Account Settings</h2>
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 overflow-auto scrollable-container">
           {/* Credit Information */}
           <div className="border border-[#E5EAEE] p-4 rounded-lg">
             <h3>Credit Usage</h3>
@@ -42,7 +42,7 @@ const SettingsScreen = () => {
               value={90}
             />
           </div>
-          <div className="col-span-2 border border-[#E5EAEE] p-4 rounded-lg flex flex-col gap-4">
+          <div className="xl:col-span-2 border border-[#E5EAEE] p-4 rounded-lg flex flex-col gap-4">
             <h3 className="text-[20px]">API Keys</h3>
             <Input title="Open AI" />
             <Input title="Anthropic" />
