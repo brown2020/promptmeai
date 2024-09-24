c0736efb2801c55c112231368b050b084e2c41bd
Muhamad Risman <real.risman@gmail.com>
2024-09-24 19:46:10 +0700

    feat: add initial Table component

--- src/screens/settings/sections/PaymentHistory.tsx
+++ src/screens/settings/sections/PaymentHistory.tsx
@@ -1,11 +1,14 @@
 import CardContent from "@/components/v2/CardContent";
+import Table from "@/components/v2/Table";
 
 const PaymentHistory = () => {
   return (
     <CardContent
       title="Payment History"
       overrideStyles="col-span-1 lg:col-span-2 xl:col-span-3"
-    ></CardContent>
+    >
+      <Table />
+    </CardContent>
   );
 };
 
