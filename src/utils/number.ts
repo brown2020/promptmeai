0cc7ad2cbda25cd57ca8c54f8f2d262879095a48
Muhamad Risman <real.risman@gmail.com>
2024-09-26 17:52:59 +0700

    fix: number helper logic

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -3,18 +3,20 @@ export const convertToSubcurrency = (amount: number, factor = 100) => {
 };
 
 export const subcurrencyToNumber = (amount: number, factor = 100) => {
-  return Math.round(amount / factor);
+  return amount / factor;
 };
 
 export const formatNumber = (
   number: number,
-  decimals: number = 2,
+  decimals: number = 0,
   currencySymbol: string = "",
   locale: string = "en-US"
 ): string => {
+  const fixedNumber = number.toFixed(decimals); // Prevents unwanted rounding
+
   return (
     currencySymbol +
-    number.toLocaleString(locale, {
+    Number(fixedNumber).toLocaleString(locale, {
       minimumFractionDigits: decimals,
       maximumFractionDigits: decimals,
     })
