86bcad1d6575be0a87955f4bd9b4a2ddfe7ac2d5
Muhamad Risman <real.risman@gmail.com>
2024-09-26 17:02:37 +0700

    feat: add subcurrencyToNumber

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -2,6 +2,10 @@ export const convertToSubcurrency = (amount: number, factor = 100) => {
   return Math.round(amount * factor);
 };
 
+export const subcurrencyToNumber = (amount: number, factor = 100) => {
+  return Math.round(amount / factor);
+};
+
 export const formatNumber = (
   number: number,
   decimals: number = 2,
