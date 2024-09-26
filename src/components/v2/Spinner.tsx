f981e5d3055856c2f640f0154bf8463fdfd260de
Muhamad Risman <real.risman@gmail.com>
2024-09-26 11:25:10 +0700

    feat: add Spinner component

---
+++ src/components/v2/Spinner.tsx
@@ -0,0 +1,7 @@
+const Spinner = () => {
+  return (
+    <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid border-transparent" />
+  );
+};
+
+export default Spinner;
