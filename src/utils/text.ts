0457d80777335dc44d7c092bbe922864b06d07b7
Muhamad Risman <real.risman@gmail.com>
2024-09-26 20:26:39 +0700

    feat: add capitalize text utils

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -5,3 +5,8 @@ export const trimText = (text: string, maxLength: number = 100): string => {
 
   return text;
 };
+
+export const capitalize = (str: string): string => {
+  if (!str) return str;
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
