fff0d4226643a0feab64eea3f25a4187e8af4364
Muhamad Risman <real.risman@gmail.com>
2024-09-26 14:56:41 +0700

    feat: add Spinner component

--- src/components/v2/Spinner.tsx
+++ src/components/v2/Spinner.tsx
@@ -1,6 +1,20 @@
-const Spinner = () => {
+type SpinnerProps = {
+  message?: string;
+};
+
+const Spinner = ({ message }: SpinnerProps) => {
   return (
-    <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid border-transparent" />
+    <div className="text-center flex flex-col gap-4 items-center justify-center">
+      {/* Spinner */}
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-gray-500 border-solid" />
+
+      {/* Message */}
+      {message && (
+        <p className="mt-4 text-lg font-medium text-gray-500 animate-pulse">
+          {message}
+        </p>
+      )}
+    </div>
   );
 };
 
