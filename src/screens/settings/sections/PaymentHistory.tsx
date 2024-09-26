00f4232595f8ba0a68cbb1c7f1efcdf6cadca8f2
Muhamad Risman <real.risman@gmail.com>
2024-09-26 20:26:57 +0700

    feat: complete payment history data integration

--- src/screens/settings/sections/PaymentHistory.tsx
+++ src/screens/settings/sections/PaymentHistory.tsx
@@ -1,13 +1,71 @@
+"use client";
+
 import CardContent from "@/components/v2/CardContent";
+import Spinner from "@/components/v2/Spinner";
 import Table from "@/components/v2/Table";
+import { formatNumber, subcurrencyToNumber } from "@/utils/number";
+import { capitalize } from "@/utils/text";
+import { useAuthStore } from "@/zustand/useAuthStore";
+import { PaymentType, usePaymentsStore } from "@/zustand/usePaymentsStore";
+import { Timestamp } from "firebase/firestore";
+import { useEffect } from "react";
+
+type TableData = {
+  no: number;
+  product: string;
+} & PaymentType;
 
 const PaymentHistory = () => {
+  const uid = useAuthStore((state) => state.uid);
+  const { payments, paymentsLoading, fetchPayments } = usePaymentsStore();
+
+  useEffect(() => {
+    if (uid) {
+      fetchPayments();
+    }
+  }, [uid, fetchPayments]);
+
+  const tableData: TableData[] = payments.map((payment, i) => ({
+    ...payment,
+    no: i + 1,
+    product: "10,000 Credits",
+  }));
+
   return (
     <CardContent
       title="Payment History"
       overrideStyles="col-span-1 lg:col-span-2 xl:col-span-3"
     >
-      <Table />
+      {paymentsLoading ? (
+        <Spinner />
+      ) : (
+        <Table
+          data={tableData}
+          headers={[
+            { key: "no", label: "No" },
+            { key: "product", label: "Product" },
+            {
+              key: "amount",
+              label: "Amount",
+              render: (amount) =>
+                formatNumber(subcurrencyToNumber(Number(amount)), 2, "$"),
+            },
+            {
+              key: "createdAt",
+              label: "Transaction Date",
+              render: (createdAt) =>
+                createdAt
+                  ? (createdAt as Timestamp).toDate().toLocaleString()
+                  : "N/A",
+            },
+            {
+              key: "status",
+              label: "Status",
+              render: (status) => capitalize(String(status)),
+            },
+          ]}
+        />
+      )}
     </CardContent>
   );
 };
