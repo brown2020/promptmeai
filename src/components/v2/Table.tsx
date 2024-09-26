6c32bfbe543c6418d723aec236a9f556cbbc5c43
Muhamad Risman <real.risman@gmail.com>
2024-09-26 20:26:29 +0700

    feat: add General Table functionalities

--- src/components/v2/Table.tsx
+++ src/components/v2/Table.tsx
@@ -1,57 +1,42 @@
-const Table = () => {
+import { ReactNode } from "react";
+
+type TableProps<T> = {
+  data: T[];
+  headers: {
+    key: keyof T;
+    label: string;
+    render?: (item: T[keyof T], row: T) => React.ReactNode;
+  }[];
+};
+
+const Table = <T extends object>({ data, headers }: TableProps<T>) => {
   return (
     <div className="relative overflow-x-auto">
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" className="px-6 py-3">
-              Product name
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Color
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Category
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Price
-            </th>
+            {headers.map((header) => (
+              <th key={String(header.key)} scope="col" className="px-6 py-3">
+                {header.label}
+              </th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <th
-              scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+          {data.map((row, rowIndex) => (
+            <tr
+              key={rowIndex}
+              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
             >
-              Apple MacBook Pro 17"
-            </th>
-            <td className="px-6 py-4">Silver</td>
-            <td className="px-6 py-4">Laptop</td>
-            <td className="px-6 py-4">$2999</td>
-          </tr>
-          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <th
-              scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-            >
-              Microsoft Surface Pro
-            </th>
-            <td className="px-6 py-4">White</td>
-            <td className="px-6 py-4">Laptop PC</td>
-            <td className="px-6 py-4">$1999</td>
-          </tr>
-          <tr className="bg-white dark:bg-gray-800">
-            <th
-              scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-            >
-              Magic Mouse 2
-            </th>
-            <td className="px-6 py-4">Black</td>
-            <td className="px-6 py-4">Accessories</td>
-            <td className="px-6 py-4">$99</td>
-          </tr>
+              {headers.map((header) => (
+                <td key={String(header.key)} className="px-6 py-4">
+                  {header.render
+                    ? header.render(row[header.key], row)
+                    : (row[header.key] as ReactNode) ?? String(row[header.key])}
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
