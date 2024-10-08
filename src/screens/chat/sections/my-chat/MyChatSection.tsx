f6814881b3068eb3d64b459114f8c4a5d5b9f4db
Muhamad Risman <real.risman@gmail.com>
2024-10-09 04:22:30 +0700

    fix: remove unsued import

--- src/screens/chat/sections/my-chat/MyChatSection.tsx
+++ src/screens/chat/sections/my-chat/MyChatSection.tsx
@@ -2,7 +2,6 @@
 
 import { ButtonIcon } from "@/components/buttons";
 import { HiOutlinePlus } from "react-icons/hi";
-import { FaArrowRight } from "react-icons/fa6";
 import ChatTabs from "./components/ChatTabs";
 import SearchInput from "./components/SearchInput";
 import ChatList from "./components/ChatList";
