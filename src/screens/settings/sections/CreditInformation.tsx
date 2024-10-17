d21ced8ea51d8ed94fdd6febf85d59a1904fcd9a
Muhamad Risman <real.risman@gmail.com>
2024-10-17 16:46:33 +0700

    fix: replace react-gauge-component since have unresolve state change issue

--- src/screens/settings/sections/CreditInformation.tsx
+++ src/screens/settings/sections/CreditInformation.tsx
@@ -3,11 +3,12 @@
 import { Button } from "@/components/buttons";
 import CardContent from "@/components/CardContent";
 import Spinner from "@/components/Spinner";
+import { cn } from "@/utils/tailwind";
 import useProfileStore, { UsageMode } from "@/zustand/useProfileStore";
 import { useUser } from "@clerk/nextjs";
+import { CircularProgress } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
-import { Fragment, useEffect, useState, useMemo } from "react";
-import { GaugeComponent } from "react-gauge-component";
+import { useEffect, useState, useMemo } from "react";
 
 const CreditInformation = () => {
   const { user } = useUser();
@@ -19,14 +20,9 @@ const CreditInformation = () => {
   const [totalCredits, setTotalCredits] = useState(0);
 
   useEffect(() => {
-    if (profile.totalCredits) {
-      setTotalCredits(profile.totalCredits);
-    }
-
-    if (profile.credits) {
-      setCredits(profile.credits);
-    }
-  }, [profile.credits, profile.totalCredits]);
+    if (profile.totalCredits) setTotalCredits(profile.totalCredits);
+    if (profile.credits) setCredits(profile.credits);
+  }, [profile]);
 
   useEffect(() => {
     if (!isDefaultData && totalCredits < credits) {
@@ -41,6 +37,10 @@ const CreditInformation = () => {
     [credits, totalCredits]
   );
 
+  const creditPercentage = useMemo(() => {
+    return totalCredits > 0 ? (credits / totalCredits) * 100 : 0;
+  }, [credits, totalCredits]);
+
   return (
     <CardContent
       title="Conversation Credits"
@@ -49,20 +49,52 @@ const CreditInformation = () => {
       {isLoading || !user || isDefaultData ? (
         <Spinner />
       ) : (
-        <Fragment>
-          <GaugeComponent
-            arc={{
-              subArcs: [
-                { limit: 20, color: "#EA4228", showTick: true },
-                { limit: 40, color: "#F58B19", showTick: true },
-                { limit: 60, color: "#F5CD19", showTick: true },
-                { limit: 100, color: "#5BE12C", showTick: true },
-              ],
+        <div className="flex flex-col items-center justify-center">
+          <CircularProgress
+            classNames={{
+              svg: "w-60 h-60 drop-shadow-md",
+              indicator: cn("stroke-default", {
+                "stroke-[#24C69E]":
+                  creditPercentage >= 60 && creditPercentage <= 100,
+                "stroke-[#F6CC19]":
+                  creditPercentage >= 40 && creditPercentage < 60,
+                "stroke-[#F58B1A]":
+                  creditPercentage >= 20 && creditPercentage < 40,
+                "stroke-[#EA4227]":
+                  creditPercentage >= 0 && creditPercentage < 20,
+              }),
+              track: cn("stroke-default/10", {
+                "stroke-[#24C69E]/10":
+                  creditPercentage >= 60 && creditPercentage <= 100,
+                "stroke-[#F6CC19]/10":
+                  creditPercentage >= 40 && creditPercentage < 60,
+                "stroke-[#F58B1A]/10":
+                  creditPercentage >= 20 && creditPercentage < 40,
+                "stroke-[#EA4227]/10":
+                  creditPercentage >= 0 && creditPercentage < 20,
+              }),
+              value: cn("text-3xl font-semibold text-default", {
+                "text-[#24C69E]":
+                  creditPercentage >= 60 && creditPercentage <= 100,
+                "text-[#F6CC19]":
+                  creditPercentage >= 40 && creditPercentage < 60,
+                "text-[#F58B1A]":
+                  creditPercentage >= 20 && creditPercentage < 40,
+                "text-[#EA4227]":
+                  creditPercentage >= 0 && creditPercentage < 20,
+              }),
             }}
-            value={(credits / totalCredits) * 100 || 0}
+            value={creditPercentage}
+            strokeWidth={4}
+            showValueLabel={true}
+            formatOptions={{
+              style: "percent",
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 2,
+            }}
           />
           <h4 className="self-center">{`Credit usage: ${creditUsage} from ${totalCredits}`}</h4>
-        </Fragment>
+        </div>
       )}
       <Button onClick={() => router.push("/payment-attempt")}>
         Buy 10,000 Credits
