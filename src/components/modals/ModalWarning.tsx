dab40c8191bc2b4e43857a9257e31fbec36fd449
Muhamad Risman <real.risman@gmail.com>
2024-10-18 03:46:20 +0700

    refactor: update ModalWarning

--- src/components/modals/ModalWarning.tsx
+++ src/components/modals/ModalWarning.tsx
@@ -1,42 +1,45 @@
 "use client";
 
-import { Modal, ModalContent, ModalBody } from "@nextui-org/react";
+import { Modal, ModalContent, ModalBody, ModalProps } from "@nextui-org/react";
 import { PiWarningFill } from "react-icons/pi";
 import { Button } from "@/components/buttons";
+import { ReactNode } from "react";
 
 type ModalWarningProps = {
   isOpen: boolean;
   title: string;
-  confirmText: string;
+  confirmText: ReactNode;
+  disableConfirm?: boolean;
   onClose?: () => void;
-};
+  onConfirm?: () => void;
+} & Omit<ModalProps, "children">;
 
 const ModalWarning = ({
   isOpen,
   title,
   confirmText,
+  disableConfirm,
   onClose,
+  onConfirm,
+  backdrop = "blur",
+  isDismissable = false,
 }: ModalWarningProps) => {
   return (
     <Modal
       isOpen={isOpen}
       onClose={onClose}
-      isDismissable={false}
+      isDismissable={isDismissable}
       hideCloseButton
-      backdrop="blur"
+      backdrop={backdrop}
     >
       <ModalContent>
-        {(onClose) => (
-          <ModalBody className="p-8 flex flex-col gap-4 items-center justify-center text-center">
-            <PiWarningFill
-              size={64}
-              color="#FF6364"
-              className="animate-pulse"
-            />
-            <span className="text-center">{title}</span>
-            <Button onClick={onClose}>{confirmText}</Button>
-          </ModalBody>
-        )}
+        <ModalBody className="p-8 flex flex-col gap-4 items-center justify-center text-center">
+          <PiWarningFill size={64} color="#FF6364" className="animate-pulse" />
+          <span className="text-center">{title}</span>
+          <Button onClick={onConfirm} disabled={disableConfirm}>
+            {confirmText}
+          </Button>
+        </ModalBody>
       </ModalContent>
     </Modal>
   );
