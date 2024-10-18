4cb6d20b1d0038e7de57b79d00e72daec48026e6
Muhamad Risman <real.risman@gmail.com>
2024-10-18 18:28:40 +0700

    feat: update chat detail controller dropdown

--- src/screens/chat/sections/chat-detail/components/ChatDetailController.tsx
+++ src/screens/chat/sections/chat-detail/components/ChatDetailController.tsx
@@ -1,11 +1,27 @@
-import { ButtonIcon } from "@/components/buttons";
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@nextui-org/react";
 import { BsThreeDots } from "react-icons/bs";
 
 const ChatDetailController = () => {
   return (
-    <div className="flex gap-[8px]">
-      <ButtonIcon icon={BsThreeDots} type="secondary" iconSize={16} />
-    </div>
+    <Dropdown>
+      <DropdownTrigger>
+        <Button isIconOnly variant="light" className="bg-[#F9F9F9] shadow-xl">
+          <BsThreeDots />
+        </Button>
+      </DropdownTrigger>
+      <DropdownMenu aria-label="Static Actions">
+        <DropdownItem key="save">Save</DropdownItem>
+        <DropdownItem key="delete" className="text-danger" color="danger">
+          Delete file
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
   );
 };
 
