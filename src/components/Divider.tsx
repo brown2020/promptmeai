a9f91da1bcae0ad5010270ae1dff1bff4c3ea64e
Muhamad Risman <real.risman@gmail.com>
2024-10-23 18:58:53 +0700

    feat: add dark mode for divider

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,5 +1,5 @@
 const Divider = () => {
-  return <div className="h-[1px] w-full bg-[#EFEFEF]" />;
+  return <div className="h-[1px] w-full bg-[#EFEFEF] dark:bg-[#2D2E30]" />;
 };
 
 export default Divider;
