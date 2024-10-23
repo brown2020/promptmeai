2db4a0fc009664bccd9474118f4779dec29d47e4
Muhamad Risman <real.risman@gmail.com>
2024-10-23 19:01:39 +0700

    feat: add dark mode for bottom panel

--- src/layouts/BottomPanel.tsx
+++ src/layouts/BottomPanel.tsx
@@ -4,7 +4,7 @@ import { NavigationList } from "./navigation-buttons";
 
 const BottomPanel = () => {
   return (
-    <div className="sm:hidden flex justify-between items-center h-[64px] flex-shrink-0 bg-white border-t border-[#EAEAEA] p-[16px]">
+    <div className="sm:hidden flex justify-between items-center h-[64px] flex-shrink-0 bg-white dark:bg-[#1E1F22] border-t border-[#EAEAEA] dark:border-[#1E1F22] p-[16px]">
       <Logo />
       <NavigationList />
       <UserProfileButton />
