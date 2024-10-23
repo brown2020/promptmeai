4af94da77dda1245e3d7bcb62112f6d07da8592d
Muhamad Risman <real.risman@gmail.com>
2024-10-23 11:02:19 +0700

    feat: implement theme provider to providers list

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,12 @@
 import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        {children}
+      </NextThemesProvider>
+    </NextUIProvider>
+  );
 }
