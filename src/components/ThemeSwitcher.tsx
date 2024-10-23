c313bad5443fcc6554b161252127269f73c11dbb
Muhamad Risman <real.risman@gmail.com>
2024-10-23 19:06:50 +0700

    feat: add dark mode on theme switcher

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -3,11 +3,11 @@ import { MdNightlightRound } from "react-icons/md";
 
 const ThemeSwitcher = () => {
   return (
-    <div className="h-[32px] w-[60px] sm:h-[60px] sm:w-[32px] bg-[#E2E2E2] rounded-xl flex sm:flex-col justify-center items-center gap-[5px]">
+    <div className="h-[32px] w-[60px] sm:h-[60px] sm:w-[32px] bg-[#E2E2E2] dark:bg-[#3F424A] rounded-xl flex sm:flex-col justify-center items-center gap-[5px]">
       <button className="w-[20px] h-[20px] flex justify-center items-center cursor-pointer">
         <MdNightlightRound size={12} />
       </button>
-      <button className="w-[20px] h-[20px] flex justify-center items-center bg-[#FFF] rounded-lg drop-shadow cursor-pointer">
+      <button className="w-[20px] h-[20px] flex justify-center items-center bg-[#FFF] dark:bg-[#64666D] rounded-lg drop-shadow cursor-pointer">
         <LuSun size={12} />
       </button>
     </div>
