0a2c9bfe21fb6e1a57022ae885ce7254d0748e8f
Muhamad Risman <real.risman@gmail.com>
2024-10-24 13:44:35 +0700

    feat: add dark mode on SettingsScreen

--- src/screens/settings/SettingsScreen.tsx
+++ src/screens/settings/SettingsScreen.tsx
@@ -8,7 +8,7 @@ const SettingsScreen = () => {
   return (
     <GreenWhiteLayout>
       <div className="flex flex-col gap-2 sm:gap-5 p-4 sm:p-8 max-w-[1320px] w-full">
-        <h2 className="font-medium text-[20px] sm:text-[24px]">
+        <h2 className="font-medium text-[20px] sm:text-[24px] dark:text-[#FFF]">
           Account Settings
         </h2>
         <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 overflow-auto scrollable-container">
