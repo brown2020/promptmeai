82c02da2572fd27df8478185738dc72b364c5cd1
Muhamad Risman <real.risman@gmail.com>
2024-10-24 13:57:26 +0700

    fix: dark mode color on CardContent

--- src/components/CardContent.tsx
+++ src/components/CardContent.tsx
@@ -16,9 +16,9 @@ const CardContent = ({
   return (
     <div
       className={cn(
-        "border border-[#E5EAEE] p-3 sm:p-4 rounded-lg flex flex-col gap-3 sm:gap-4 transition duration-300 ease",
+        "border border-[#E5EAEE] dark:border-[#6B7280] p-3 sm:p-4 rounded-lg flex flex-col gap-3 sm:gap-4 transition duration-300 ease",
         {
-          "border-[#0CA37F] border-[2px]": isActive,
+          "border-[#0CA37F] dark:border-[#0CA37F] border-[2px]": isActive,
         },
         overrideStyles
       )}
