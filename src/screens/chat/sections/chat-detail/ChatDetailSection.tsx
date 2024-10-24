c6f3b25f40ddf2674b4ee65ced3fe87f2ba6757d
Muhamad Risman <real.risman@gmail.com>
2024-10-24 11:21:50 +0700

    feat: add dark mode for ChatDetailSection

--- src/screens/chat/sections/chat-detail/ChatDetailSection.tsx
+++ src/screens/chat/sections/chat-detail/ChatDetailSection.tsx
@@ -43,13 +43,13 @@ const ChatDetailSection = () => {
           className="lg:hidden cursor-pointer flex-shrink-0"
           onClick={() => setDrawerOpen(true)}
         />
-        <h3 className="text-[18px] whitespace-nowrap overflow-hidden text-ellipsis text-[#1E1F22]">
+        <h3 className="text-[18px] whitespace-nowrap overflow-hidden text-ellipsis text-[#1E1F22] dark:text-[#EEE]">
           {isNewChat ? typedTitle : title}
         </h3>
         <ChatDetailActions />
       </div>
       {/* Main chat detail section */}
-      <div className="bg-[#F5F5F5] h-[calc(100%-48px)] w-[calc(100% + 32px)] rounded-lg lg:ml-[-16px] flex justify-center px-[18px] sm:px-[24px]">
+      <div className="bg-[#F5F5F5] dark:bg-[#3F424A] h-[calc(100%-48px)] w-[calc(100% + 32px)] rounded-lg lg:ml-[-16px] flex justify-center px-[18px] sm:px-[24px]">
         {/* Wrapper */}
         <div className="h-full w-full max-w-[736px] flex flex-col gap-[18px] sm:gap-[24px] justify-between items-center py-[18px] sm:py-[24px]">
           <ChatResponseList />
