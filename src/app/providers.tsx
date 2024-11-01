16c33472f9ca5f18f1628e432a4e948695763ce7
Muhamad Risman <real.risman@gmail.com>
2024-11-01 20:14:18 +0700

    feat: registeri session provider and firebase sync auth provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,18 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import ClerkAppProvider from "./clerk-provider";
+import FirebaseAuthProvider from "./firebase-auth-provider";
+import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="system">
-        <ClerkAppProvider>{children}</ClerkAppProvider>
+        <ClerkAppProvider>
+          <SessionProvider>
+            <FirebaseAuthProvider>{children}</FirebaseAuthProvider>
+          </SessionProvider>
+        </ClerkAppProvider>
       </NextThemesProvider>
     </NextUIProvider>
   );
