c1d75c474d1f7d28150a18ec565b191b66b7b492
Muhamad Risman <real.risman@gmail.com>
2024-11-04 13:43:11 +0700

    feat: add page complete signin

---
+++ src/app/auth/complete-signin/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { auth } from "@/firebase/firebaseClient";
+import { isSignInWithEmailLink, signInWithEmailLink } from "firebase/auth";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const CompleteSignIn = () => {
+  const router = useRouter();
+  const [message, setMessage] = useState<string>("Completing sign-in...");
+
+  useEffect(() => {
+    const completeSignIn = async () => {
+      if (isSignInWithEmailLink(auth, window.location.href)) {
+        let email = window.localStorage.getItem("emailForSignIn");
+        if (!email) {
+          email =
+            window.prompt("Please provide your email for confirmation") || "";
+        }
+
+        try {
+          const result = await signInWithEmailLink(
+            auth,
+            email,
+            window.location.href
+          );
+
+          await signIn("credentials", { email: result.user?.email || "" });
+          setMessage("Sign-in complete! Redirecting...");
+          router.push("/chat");
+        } catch (error) {
+          console.error("Error completing sign-in: ", error);
+          setMessage("Failed to complete sign-in.");
+        }
+      }
+    };
+
+    completeSignIn();
+  }, [router]);
+
+  return <p>{message}</p>;
+};
+
+export default CompleteSignIn;
