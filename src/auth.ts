c0e3b0019a9682ed5e840fe08c471c062d35d4e6
Muhamad Risman <real.risman@gmail.com>
2024-11-09 22:44:41 +0700

    refactor: auth config to seperate the providers

--- src/auth.ts
+++ src/auth.ts
@@ -3,37 +3,51 @@ import Google from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
 import { admin, adminAuth, adminDb } from "./firebase/firebaseAdmin";
+import type { Provider } from "next-auth/providers";
+
+const providers: Provider[] = [
+  Google,
+  Credentials({
+    name: "Email Link",
+    credentials: {
+      email: { label: "Email", type: "text" },
+      accessToken: { label: "Access Token", type: "text" },
+    },
+    authorize: async (credentials) => {
+      const { email, accessToken } = credentials;
+
+      console.log("proccess email", email);
+      console.log("proccess accessToken", accessToken);
+
+      try {
+        const decodedToken = await admin
+          .auth()
+          .verifyIdToken(accessToken as string);
+
+        console.log("Token verified successfully", decodedToken);
+
+        return null;
+      } catch (error) {
+        console.error("Error verifying token:", error);
+        throw error;
+      }
+    },
+  }),
+];
+
+export const providerMap = providers
+  .map((provider) => {
+    if (typeof provider === "function") {
+      const providerData = provider();
+      return { id: providerData.id, name: providerData.name };
+    } else {
+      return { id: provider.id, name: provider.name };
+    }
+  })
+  .filter((provider) => provider.id !== "credentials");
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Google,
-    Credentials({
-      name: "Email Link",
-      credentials: {
-        email: { label: "Email", type: "text" },
-        accessToken: { label: "Access Token", type: "text" },
-      },
-      authorize: async (credentials) => {
-        const { email, accessToken } = credentials;
-
-        console.log("proccess email", email);
-        console.log("proccess accessToken", accessToken);
-
-        try {
-          const decodedToken = await admin
-            .auth()
-            .verifyIdToken(accessToken as string);
-
-          console.log("Token verified successfully", decodedToken);
-
-          return null;
-        } catch (error) {
-          console.error("Error verifying token:", error);
-          throw error;
-        }
-      },
-    }),
-  ],
+  providers,
   session: {
     strategy: "jwt",
   },
