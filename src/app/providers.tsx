dda518da53a854e022a2001dd7d560f829667bc5
hiren.surani@qubited.com <hiren.surani@qubited.com>
2024-11-12 09:57:08 +0530

    Working on add auth component

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,39 @@
+"use client";
+
 import { NextUIProvider } from "@nextui-org/react";
+import CookieConsent from "react-cookie-consent";
+import { usePathname, useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import ClerkAppProvider from "./clerk-provider";
+import useAuthToken from "@/hooks/useAuthToken";
+import { useEffect } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const { loading, uid } = useAuthToken(process.env.NEXT_PUBLIC_COOKIE_NAME!);
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (
+      !loading &&
+      !uid &&
+      pathname != "/" &&
+      !pathname.includes("/about") &&
+      !pathname.includes("/terms") &&
+      !pathname.includes("/privacy") &&
+      !pathname.includes("/support")
+    ) {
+      router.push("/");
+    }
+  }, [loading, pathname, router, uid]);
+
   return (
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="system">
-        <ClerkAppProvider>{children}</ClerkAppProvider>
+        <ClerkAppProvider>{children}</ClerkAppProvider>{" "}
+        <CookieConsent>
+          This app uses cookies to enhance the user experience.
+        </CookieConsent>
       </NextThemesProvider>
     </NextUIProvider>
   );
