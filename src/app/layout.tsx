c896210a2a49eaa8551dad2c17841130fbce52d4
Muhamad Risman <real.risman@gmail.com>
2024-11-21 01:16:08 +0700

    fix: remove unsued migration scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,6 @@ import Layout from "@/layouts/Layout";
 import { Toaster } from "react-hot-toast";
 
 import "@/styles/globals.css";
-import { auth } from "@/auth";
-import {
-  fetchUsersByAuthEmail,
-  moveCollection,
-  moveDocumentWithoutFields,
-} from "@/utils/migration";
 import Providers from "@/providers";
 
 export const metadata = {
@@ -20,33 +14,6 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const session = await auth();
-
-  // console.log("session", session);
-
-  // const foundUsers = await fetchUsersByAuthEmail("real.risman@gmail.com");
-
-  // if (session?.user?.id && foundUsers.length > 0) {
-  //   const oldId = foundUsers[0].id;
-  //   const newId = session.user.id;
-
-  //   await moveDocumentWithoutFields(
-  //     `users/${oldId}/profile/userData`,
-  //     `users/${newId}`,
-  //     ["contactEmail", "displayName", "email", "emailVerified", "photoUrl"]
-  //   );
-
-  //   await moveCollection(
-  //     `users/${oldId}/payments`,
-  //     `payments/${newId}/transactions`
-  //   );
-
-  //   await moveCollection(
-  //     `promptme_chats/${oldId}/chat`,
-  //     `promptme_chats/${newId}/chats`
-  //   );
-  // }
-
   return (
     <html lang="en">
       <body>
