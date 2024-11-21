f256d05e464f1136c59d6ac003737f4925f15586
Muhamad Risman <real.risman@gmail.com>
2024-11-21 22:01:41 +0700

    feat: add initial UserProfileButton logic

--- src/components/UserProfileButton.tsx
+++ src/components/UserProfileButton.tsx
@@ -1,24 +1,99 @@
 "use client";
 
-import { User } from "next-auth";
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownSection,
+  DropdownTrigger,
+  User,
+} from "@nextui-org/react";
+import { User as UserType } from "next-auth";
 import Image from "next/image";
 import { FaUserAstronaut } from "react-icons/fa";
+import { useWindowSize } from "react-use";
+import ThemeSwitcher from "./ThemeSwitcher";
 
 type UserProfileButtonProps = {
-  user?: User;
+  user?: UserType;
 };
 
 const UserProfileButton = ({ user }: UserProfileButtonProps) => {
-  return user && user?.image ? (
-    <Image
-      src={user?.image}
-      width="32"
-      height="32"
-      alt="Profile Image"
-      className="cursor-pointer rounded-full hover:border hover:border-neutral-300"
-    />
-  ) : (
-    <FaUserAstronaut size={24} color="#255148" className="cursor-pointer" />
+  const { width } = useWindowSize();
+
+  return (
+    <Dropdown>
+      <DropdownTrigger>
+        {user?.image ? (
+          <Image
+            src={user?.image}
+            width="32"
+            height="32"
+            alt="Profile Image"
+            className="cursor-pointer rounded-full hover:outline hover:outline-2 hover:outline-[#0CA37F]"
+          />
+        ) : (
+          <FaUserAstronaut
+            size={24}
+            color="#255148"
+            className="cursor-pointer"
+          />
+        )}
+      </DropdownTrigger>
+      <DropdownMenu
+        aria-label="Profile menu list"
+        itemClasses={{
+          base: [
+            "rounded-md",
+            "text-default-500",
+            "transition-opacity",
+            "data-[hover=true]:text-foreground",
+            "data-[hover=true]:bg-default-100",
+            "dark:data-[hover=true]:bg-default-50",
+            "data-[selectable=true]:focus:bg-default-50",
+            "data-[pressed=true]:opacity-70",
+            "data-[focus-visible=true]:ring-default-500",
+          ],
+        }}
+      >
+        <DropdownSection
+          aria-label="Profile & Actions"
+          showDivider={width < 640}
+        >
+          <DropdownItem
+            isReadOnly
+            key="profile"
+            className="h-14 gap-2 opacity-100"
+          >
+            <User
+              name={user?.name || ""}
+              classNames={{
+                name: "text-default-600",
+                description: "text-default-500",
+              }}
+              avatarProps={{
+                size: "sm",
+                src: user?.image || "",
+              }}
+            />
+          </DropdownItem>
+
+          <DropdownItem key="manage-account">Manage Account</DropdownItem>
+
+          <DropdownItem
+            isReadOnly
+            key="theme"
+            className="cursor-default flex sm:hidden"
+            endContent={<ThemeSwitcher />}
+          >
+            Theme
+          </DropdownItem>
+        </DropdownSection>
+        <DropdownSection aria-label="Profile & Actions">
+          <DropdownItem key="logout">Log Out</DropdownItem>
+        </DropdownSection>
+      </DropdownMenu>
+    </Dropdown>
   );
 };
 
