5c1a8da2b32e4b37b318f64f5b10b151dcb963f8
Muhamad Risman <real.risman@gmail.com>
2024-11-23 08:00:50 +0700

    feat: add ability to control manage profile form

--- src/components/modals/ManageProfile.tsx
+++ src/components/modals/ManageProfile.tsx
@@ -12,6 +12,8 @@ import { User } from "next-auth";
 import Image from "next/image";
 import { Button } from "../buttons";
 import { CgTrash } from "react-icons/cg";
+import { HiOutlinePencilAlt } from "react-icons/hi";
+import { useState } from "react";
 
 type ManageProfileProps = {
   user?: User;
@@ -21,6 +23,9 @@ type ManageProfileProps = {
 } & Omit<ModalProps, "children">;
 
 const ManageProfile = ({ user, isOpen, onClose }: ManageProfileProps) => {
+  const [name, setName] = useState<string>(user?.name || "");
+  const [email, setEmail] = useState<string>(user?.email || "");
+
   return (
     <Modal
       isOpen={isOpen}
@@ -46,16 +51,32 @@ const ManageProfile = ({ user, isOpen, onClose }: ManageProfileProps) => {
                 className="rounded-full absolute top-[36px] m-auto left-0 right-0 outline outline-2 outline-[#FFF] drop-shadow"
               />
               <div className="mt-6 flex flex-col gap-2">
-                <Input type="string" label="Name" value={user?.name || ""} />
-                <Input type="email" label="Email" value={user?.email || ""} />
+                <Input
+                  type="string"
+                  label="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  endContent={<HiOutlinePencilAlt color="#52525B" />}
+                  variant="bordered"
+                />
+                <Input
+                  type="email"
+                  label="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  endContent={<HiOutlinePencilAlt color="#52525B" />}
+                  variant="bordered"
+                />
               </div>
               <div className="flex justify-between items-center pt-4">
                 <button className="flex gap-1 items-center bg-white dark:bg-gray-800 text-red-500 dark:text-red-400 py-2 px-4 rounded hover:bg-red-50 dark:hover:bg-gray-700 transition duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2 focus:ring-offset-gray-100 dark:focus:ring-offset-gray-900">
-                  <CgTrash size={18} /> Delete
+                  <CgTrash size={17} /> Delete
                 </button>
                 <div className="flex gap-2 items-center">
-                  <Button variant="text">Cancel</Button>
-                  <Button disabled>Save Changes</Button>
+                  <Button variant="text" onClick={onClose}>
+                    Cancel
+                  </Button>
+                  <Button>Save Changes</Button>
                 </div>
               </div>
             </ModalBody>
