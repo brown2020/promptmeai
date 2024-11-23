9546aa358382b60866f5d32f3108404707666862
Muhamad Risman <real.risman@gmail.com>
2024-11-23 08:59:32 +0700

    fix: dark mode issue for manage profile

--- src/components/modals/ManageProfile.tsx
+++ src/components/modals/ManageProfile.tsx
@@ -34,21 +34,22 @@ const ManageProfile = ({ user, isOpen, onClose }: ManageProfileProps) => {
       hideCloseButton
       radius="lg"
       classNames={{
-        base: "bg-[#F1F1F1] relative",
+        base: "bg-[#F1F1F1] dark:bg-[#0F0E10] relative",
         body: "py-6",
       }}
+      placement="center"
     >
       <ModalContent>
         {(onClose) => (
           <>
-            <ModalHeader className="border-b flex justify-between items-center h-[72px]" />
-            <ModalBody className="bg-[#FFF]">
+            <ModalHeader className="border-b dark:border-[#19181B] flex justify-between items-center h-[72px]" />
+            <ModalBody className="bg-[#FFF] dark:bg-[#19181B]">
               <Image
                 src={user?.image || ""}
                 alt="User Image"
                 width={72}
                 height={72}
-                className="rounded-full absolute top-[36px] m-auto left-0 right-0 outline outline-2 outline-[#FFF] drop-shadow"
+                className="rounded-full absolute top-[36px] m-auto left-0 right-0 outline outline-2 outline-[#FFF] dark:outline-[#19181B] drop-shadow"
               />
               <div className="mt-6 flex flex-col gap-2">
                 <Input
@@ -57,7 +58,6 @@ const ManageProfile = ({ user, isOpen, onClose }: ManageProfileProps) => {
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                   endContent={<HiOutlinePencilAlt color="#52525B" />}
-                  variant="bordered"
                 />
                 <Input
                   type="email"
@@ -65,11 +65,10 @@ const ManageProfile = ({ user, isOpen, onClose }: ManageProfileProps) => {
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   endContent={<HiOutlinePencilAlt color="#52525B" />}
-                  variant="bordered"
                 />
               </div>
               <div className="flex justify-between items-center pt-4">
-                <button className="flex gap-1 items-center bg-white dark:bg-gray-800 text-red-500 dark:text-red-400 py-2 px-4 rounded hover:bg-red-50 dark:hover:bg-gray-700 transition duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2 focus:ring-offset-gray-100 dark:focus:ring-offset-gray-900">
+                <button className="flex gap-1 items-center bg-white dark:bg-[#19181B] text-red-500 dark:text-red-400 py-2 px-4 rounded hover:bg-red-50 dark:hover:bg-gray-700 transition duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2 focus:ring-offset-gray-100 dark:focus:ring-offset-gray-900">
                   <CgTrash size={17} /> Delete
                 </button>
                 <div className="flex gap-2 items-center">
