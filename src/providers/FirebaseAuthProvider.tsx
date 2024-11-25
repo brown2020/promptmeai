daede985dbf2f01dc94ca9c4f416174feec8cb9b
Muhamad Risman <real.risman@gmail.com>
2024-11-25 17:11:25 +0700

    fix: fetch profile logic

--- src/providers/FirebaseAuthProvider.tsx
+++ src/providers/FirebaseAuthProvider.tsx
@@ -1,39 +1,40 @@
 "use client";
 
-import { PropsWithChildren, useEffect } from "react";
+import { PropsWithChildren, useCallback, useEffect } from "react";
 import { signOut, useSession } from "next-auth/react";
 import { Session } from "next-auth";
 import { signInWithCustomToken } from "firebase/auth";
 import { auth } from "@/firebase/firebaseClient";
 import useProfileStore from "@/zustand/useProfileStore";
 
-const syncFirebaseAuth = async (session: Session) => {
-  if (session && session.firebaseToken) {
-    try {
-      await signInWithCustomToken(auth, session.firebaseToken);
-    } catch (error) {
-      console.error("Error signing in with custom token: ", error);
-    }
-  } else {
-    signOut();
-    auth.signOut();
-  }
-};
-
 const FirebaseAuthProvider = ({ children }: PropsWithChildren) => {
   const { fetchProfile } = useProfileStore();
   const { data: session } = useSession();
   const userId = session?.user?.id;
 
+  const syncFirebaseAuth = useCallback(
+    async (session: Session) => {
+      if (session && session.firebaseToken && userId) {
+        try {
+          await signInWithCustomToken(auth, session.firebaseToken);
+
+          fetchProfile(userId);
+        } catch (error) {
+          console.error("Error signing in with custom token: ", error);
+        }
+      } else {
+        signOut();
+        auth.signOut();
+      }
+    },
+    [fetchProfile, userId]
+  );
+
   useEffect(() => {
     if (!session) return;
 
     syncFirebaseAuth(session);
-
-    if (userId) {
-      fetchProfile(userId);
-    }
-  }, [fetchProfile, session, userId]);
+  }, [session, syncFirebaseAuth]);
 
   return <>{children}</>;
 };
