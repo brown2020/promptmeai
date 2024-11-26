613a61a75a1eb18be825bf36766d1c92f69351d3
Muhamad Risman <real.risman@gmail.com>
2024-11-26 19:44:24 +0700

    feat: add actions authenticateAccountRegister

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -3,6 +3,11 @@
 import { signIn } from "@/auth";
 import { AuthError } from "next-auth";
 
+type AuthResult = {
+  success: boolean; // Indicates if the action was successful
+  message?: string; // Contains an error message if applicable
+};
+
 export async function authenticateLoginPassword(
   prevState: string | undefined,
   formData: FormData
@@ -22,5 +27,28 @@ export async function authenticateLoginPassword(
       }
     }
     throw error;
+  }
+}
+
+export async function authenticateAccountRegister(
+  prevState: AuthResult | undefined,
+  formData: FormData
+): Promise<AuthResult> {
+  try {
+    await signIn("account-register", formData);
+    return { success: true };
+  } catch (error) {
+    if (error instanceof AuthError) {
+      if (error.cause?.err instanceof Error) {
+        return { success: false, message: error.cause.err.message };
+      }
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { success: false, message: "Invalid credentials" };
+        default:
+          return { success: false, message: "Something went wrong" };
+      }
+    }
+    throw error; // Re-throw non-AuthError cases
   }
 }
