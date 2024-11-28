3f66848d9d0eb426e0d1f4ff20a31b011b1a1f41
Muhamad Risman <real.risman@gmail.com>
2024-11-29 05:08:40 +0700

    feat: add ability to persist auth user on sync firebase auth

--- src/providers/FirebaseAuthProvider.tsx
+++ src/providers/FirebaseAuthProvider.tsx
@@ -1,40 +1,54 @@
 "use client";
 
 import { PropsWithChildren, useCallback, useEffect } from "react";
-import { signOut, useSession } from "next-auth/react";
+import { signOut as nextAuthSignOut, useSession } from "next-auth/react";
 import { Session } from "next-auth";
 import { signInWithCustomToken } from "firebase/auth";
-import { auth } from "@/firebase/firebaseClient";
 import useProfileStore from "@/zustand/useProfileStore";
+import { useAuthStore } from "@/zustand/useAuthStore";
+import { auth } from "@/firebase/firebaseClient";
 
 const FirebaseAuthProvider = ({ children }: PropsWithChildren) => {
   const { fetchProfile } = useProfileStore();
+  const { setUser, logout } = useAuthStore();
   const { data: session } = useSession();
   const userId = session?.user?.id;
 
   const syncFirebaseAuth = useCallback(
     async (session: Session) => {
-      if (session && session.firebaseToken && userId) {
+      if (session?.firebaseToken && userId) {
         try {
-          await signInWithCustomToken(auth, session.firebaseToken);
+          // Sign in with the custom token from NextAuth
+          const userCredential = await signInWithCustomToken(
+            auth,
+            session.firebaseToken
+          );
 
+          // Set the user in Zustand (user is available in userCredential.user)
+          setUser(userCredential.user);
+
+          // Fetch the user profile once authenticated
           fetchProfile(userId);
         } catch (error) {
           console.error("Error signing in with custom token: ", error);
         }
       } else {
-        signOut();
-        auth.signOut();
+        // Sign out of both Firebase and NextAuth
+        await nextAuthSignOut();
+        await auth.signOut();
+        logout(); // Clear the user from Zustand
       }
     },
-    [fetchProfile, userId]
+    [fetchProfile, logout, setUser, userId]
   );
 
   useEffect(() => {
-    if (!session) return;
-
+    if (!session) {
+      logout(); // Ensure Zustand is cleared if no session
+      return;
+    }
     syncFirebaseAuth(session);
-  }, [session, syncFirebaseAuth]);
+  }, [session, syncFirebaseAuth, logout]);
 
   return <>{children}</>;
 };
