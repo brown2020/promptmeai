e9915edaf2807ce080ab33ae564d71c159efe401
Muhamad Risman <real.risman@gmail.com>
2024-11-30 18:02:26 +0700

    fix: logic firebase sync authentication

--- src/providers/FirebaseAuthProvider.tsx
+++ src/providers/FirebaseAuthProvider.tsx
@@ -1,31 +1,24 @@
 "use client";
 
-import { PropsWithChildren, useCallback, useEffect } from "react";
+import { PropsWithChildren, useEffect } from "react";
 import { signOut as nextAuthSignOut, useSession } from "next-auth/react";
-import { Session } from "next-auth";
 import { signInWithCustomToken } from "firebase/auth";
 import useProfileStore from "@/zustand/useProfileStore";
-import { useAuthStore } from "@/zustand/useAuthStore";
 import { auth } from "@/firebase/firebaseClient";
 
 const FirebaseAuthProvider = ({ children }: PropsWithChildren) => {
-  const { fetchProfile } = useProfileStore();
-  const { setUser, logout } = useAuthStore();
+  const { fetchProfile, profile } = useProfileStore();
   const { data: session } = useSession();
-  const userId = session?.user?.id;
 
-  const syncFirebaseAuth = useCallback(
-    async (session: Session) => {
-      if (session?.firebaseToken && userId) {
+  useEffect(() => {
+    const syncAuth = async (
+      firebaseToken: string | undefined,
+      userId: string | undefined
+    ) => {
+      if (firebaseToken && userId) {
         try {
           // Sign in with the custom token from NextAuth
-          const userCredential = await signInWithCustomToken(
-            auth,
-            session.firebaseToken
-          );
-
-          // Set the user in Zustand (user is available in userCredential.user)
-          setUser(userCredential.user);
+          await signInWithCustomToken(auth, firebaseToken);
 
           // Fetch the user profile once authenticated
           fetchProfile(userId);
@@ -36,19 +29,14 @@ const FirebaseAuthProvider = ({ children }: PropsWithChildren) => {
         // Sign out of both Firebase and NextAuth
         await nextAuthSignOut();
         await auth.signOut();
-        logout(); // Clear the user from Zustand
       }
-    },
-    [fetchProfile, logout, setUser, userId]
-  );
+    };
 
-  useEffect(() => {
-    if (!session) {
-      logout(); // Ensure Zustand is cleared if no session
-      return;
+    // Add profile email checker to indicate the user is already authenticated
+    if (session && !profile.email) {
+      syncAuth(session.firebaseToken, session.user?.id);
     }
-    syncFirebaseAuth(session);
-  }, [session, syncFirebaseAuth, logout]);
+  }, [fetchProfile, profile.email, session]);
 
   return <>{children}</>;
 };
